933779fca6de626941cb2da7fd9faf5bf206258e
riahirami <riahirami91@gmail.com>
2023-07-05 18:37:58 +0100

    inverse seed role & admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,9 +19,9 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
+        $this->call(RoleSeeder::class);
         $this->call(AdminSeeder::class);
         $this->call(CategorySeeder::class);
-        $this->call(RoleSeeder::class);
 
 
     }
